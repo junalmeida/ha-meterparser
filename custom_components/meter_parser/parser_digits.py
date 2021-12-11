bc85ff6d1da226cfd5a5f67f6e92055a9e313454
Marcos Junior <marcos.junior@permissionclick.com>
2021-12-11 12:21:36 -0600

    Back to open space ocr

--- custom_components/meter_parser/parser_digits.py
+++ custom_components/meter_parser/parser_digits.py
@@ -21,82 +21,65 @@ import requests
 import cv2
 import urllib.parse
 import numpy as np
+import re
 
 _LOGGER = logging.getLogger(__name__)
-MS_API = "/vision/v3.2/ocr"
+URL_API = "https://api.ocr.space/parse/image"
 
 
 def parse_digits(
-    frame,
+    image,
     digits_count: int,
     ocr_key: str,
-    ocr_url: str,
     entity_id: str,
     debug_path: str = None,
 ):
     """Displaying digits and OCR"""
-    # remove red colors
-    lower = (200, 0, 0)  # lower bound for each channel
-    upper = (255, 0, 0)  # upper bound for each channel
-    mask = cv2.inRange(frame, lower, upper)
-    frame[mask != 0] = [255, 255, 255]
-
-    # Adjust the exposure
-    alpha = float(2.5)
-    blur = int(3)
-    threshold = 37
-    adjustment = 11
-    frame = cv2.multiply(frame, np.array([alpha]))
-    # Convert to grayscale
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Blur to reduce noise
-    frame = cv2.GaussianBlur(frame, (blur, blur), 0)
-    # Threshold the image
-    frame = cv2.adaptiveThreshold(
-        frame,
-        255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY,
-        threshold,
-        adjustment,
-    )
-
     debugfile = time.strftime(entity_id + "-%Y-%m-%d_%H-%M-%S")
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # remove red colors
+    sensitivity = 30
+    lower_red_0 = np.array([0, 100, 100])
+    upper_red_0 = np.array([sensitivity, 255, 255])
+    lower_red_1 = np.array([180 - sensitivity, 100, 100])
+    upper_red_1 = np.array([180, 255, 255])
+
+    mask_0 = cv2.inRange(hsv, lower_red_0, upper_red_0)
+    mask_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+
+    mask = cv2.bitwise_or(mask_0, mask_1)
+    # Change image to white where we found red
+    frame = image.copy()
+    frame[np.where(mask)] = (255, 255, 255)
+
     if debug_path is not None:
         cv2.imwrite(os.path.join(debug_path, "%s-in.jpg" % debugfile), frame)
 
-    query_params = {
-        # Request parameters
-        "language": "en",
-        "detectOrientation": "false",
-        "model-version": "latest",
+    payload = {
+        "apikey": ocr_key,
+        "language": "eng",
+        "scale": "true",
+        "OCREngine": "2"
     }
-    header_params = {
-        "Accept": "application/json",
-        "Content-Type": "application/octet-stream",
-        "Ocp-Apim-Subscription-Key": ocr_key,
-    }
-    ocr_url = urllib.parse.urljoin(ocr_url, MS_API)
 
-    _LOGGER.debug("%s: OCR image: %s" % (entity_id, ocr_url))
+    _LOGGER.debug("%s: OCR image: %s" % (entity_id, URL_API))
     imencoded = cv2.imencode(".jpg", frame)[1]
     response = requests.post(
-        ocr_url,
-        data=imencoded.tobytes(),
-        params=query_params,
-        headers=header_params,
+        URL_API,
+        data=payload,
+        files={"file.jpg": imencoded.tobytes()},
         timeout=9,
     )
 
     if response.status_code == 200:
         result = response.json()
-        if "regions" in result:
+        if "ParsedResults" in result:
             text = ""
-            for r in result["regions"]:
-                for l in r["lines"]:
-                    for w in l["words"]:
-                        text = text + w["text"]
-                    text = text + "\n"
+            for reg in result["ParsedResults"]:
+                if "ParsedText" in reg:
+                    text = text + reg["ParsedText"]
+                text = text + "\n"
             return parse_result(
                 text,
                 digits_count,
@@ -113,8 +96,9 @@ def parse_result(ocr: str, digits: int, entity_id: str):
     if ocr is not None and ocr != "":
         array = ocr.strip().split("\n")
         for x_str in array:
-            x_str = x_str.replace(" ", "").replace(".", "").replace(",", "")
-            if len(x_str) == digits and x_str.isnumeric():
-                _LOGGER.debug("%s: Final reading: %s" % (entity_id, x_str))
-                return x_str
+            x_str = x_str.replace(" ", "").replace(".", "").replace(",", "").replace("|", "").replace("/", "").replace("\\", "").replace("o", "0").replace("O", "0")  # replace common ocr mistakes
+            x_str = re.search("[0-9]{%s}" % digits, x_str)
+            if x_str is not None and x_str.group(0) is not None:
+                _LOGGER.debug("%s: Final reading: %s" % (entity_id, x_str.group(0)))
+                return x_str.group(0)
     raise Exception("Unable to OCR image or no text found.")
--- tests/custom_components/meterparser/test_parse_image.py
+++ tests/custom_components/meterparser/test_parse_image.py
@@ -4,79 +4,94 @@ import cv2
 from custom_components.meter_parser.parser_dial import parse_dials
 from custom_components.meter_parser.parser_digits import parse_digits
 
+ocr_key = "890a9b9b8388957"
+entity_id = "test.test"
+dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "results")
+
 
 def test_eletricity_dials_1():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_eletricity-1.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], minDiameter=200, maxDiameter=300, debug=True)
+    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], entity_id, minDiameter=200, maxDiameter=300, debug_path=dir_path)
     assert reading == '3124'
 
 
 def test_eletricity_dials_2():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_eletricity-2.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], minDiameter=200, maxDiameter=300, debug=True)
+    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], entity_id, minDiameter=200, maxDiameter=300, debug_path=dir_path)
     assert reading == '2211'
 
 
 def test_eletricity_dials_3():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_eletricity-3.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CW", "CCW", "CW", "CCW", "CW"], minDiameter=200, maxDiameter=300, debug=True)
+    reading = parse_dials(inputFrame, ["CW", "CCW", "CW", "CCW", "CW"], entity_id, minDiameter=200, maxDiameter=300, debug_path=dir_path)
     assert reading == '68451'
 
 
 def test_gas_dials_1():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_gas-1.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], minDiameter=300, maxDiameter=400, debug=True)
+    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], entity_id, minDiameter=300, maxDiameter=400, debug_path=dir_path)
     assert reading == '3876'
 
 
 def test_gas_dials_2():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_gas-2.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], minDiameter=290, maxDiameter=330, debug=True)
+    reading = parse_dials(inputFrame, ["CCW", "CW", "CCW", "CW"], entity_id, minDiameter=290, maxDiameter=330, debug_path=dir_path)
     assert reading == '0140'
 
 
 def test_water_dial_1():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-1.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CW"], minDiameter=640, maxDiameter=840, debug=True)
+    reading = parse_dials(inputFrame, ["CW"], entity_id, minDiameter=640, maxDiameter=840, debug_path=dir_path)
     assert reading == '0'
 
 
 def test_water_dial_2():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-2.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CW"], minDiameter=1010, maxDiameter=1400, debug=True)
+    reading = parse_dials(inputFrame, ["CW"], entity_id, minDiameter=1010, maxDiameter=1400, debug_path=dir_path)
     assert reading == '1'
 
 
 def test_water_dial_3():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-3.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_dials(inputFrame, ["CW"], minDiameter=1010, maxDiameter=1230, debug=True)
+    reading = parse_dials(inputFrame, ["CW"], entity_id, minDiameter=1010, maxDiameter=1230, debug_path=dir_path)
     assert reading == '5'
 
 
 def test_water_digits_1():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-1.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_digits(inputFrame, 6)
-    assert reading == '000200'
+    inputFrame = crop(inputFrame, [179, 155, 194, 50])
+    reading = parse_digits(inputFrame, 6, ocr_key, entity_id, debug_path=dir_path)
+    assert reading == '028020'
 
 
 def test_water_digits_2():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-2.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_digits(inputFrame, 6)
+    inputFrame = crop(inputFrame, [40, 144, 118, 50])
+    reading = parse_digits(inputFrame, 6, ocr_key, entity_id, debug_path=dir_path)
     assert reading == '000110'
 
 
 def test_water_digits_3():
     samplepath = os.path.join(os.path.dirname(__file__), 'sample_water-3.jpg')
     inputFrame = cv2.imread(samplepath)
-    reading = parse_digits(inputFrame, 6)
-    assert reading == '027795'
+    inputFrame = crop(inputFrame, [451, 414, 372, 133])
+    reading = parse_digits(inputFrame, 6, ocr_key, entity_id, debug_path=dir_path)
+    assert reading == '000200'
+
+
+def crop(image, rect):
+    x = rect[0]
+    y = rect[1]
+    w = rect[2]
+    h = rect[3]
+    return image[y: y + h, x: x + w]
